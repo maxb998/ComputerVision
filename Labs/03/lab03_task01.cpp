3e78c50543efe5bde379a68943e7973205433d28
maxb998 <boldrinmassimo98@gmail.com>
2022-04-01 10:08:27 +0200

    Messing with cmake #2

--- Labs/03/lab03_task01.cpp
+++ Labs/03/lab03_task01.cpp
@@ -1,12 +1,10 @@
 #include <opencv2/highgui.hpp>
-#include <filesystem>
 
 using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv)
 {
-    //filesystem::path p = 
     Mat img = cv::imread("robocup.jpg");
 
     imshow("Robocup", img);
--- Labs/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/massimo/Documents/GitHub/ComputerVision/Labs
-# Build directory: /home/massimo/Documents/GitHub/ComputerVision/Labs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
