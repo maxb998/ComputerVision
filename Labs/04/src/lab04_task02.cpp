92242646f4f901de7a2b9c3b7e25a597ff5d8b13
maxb998 <boldrinmassimo98@gmail.com>
2022-04-08 17:04:12 +0200

    Lab04: Added support for default image path

--- Labs/04/src/lab04_task02.cpp
+++ Labs/04/src/lab04_task02.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 Mat myFindStraightlines(Mat img) // had to specify it was mine because there is one with the same name in openCV Mat
 {
-    Mat enhancedImg = Mat::zeros(img.size(), CV_8UC1);
+    Mat enhancedImg = img.clone();
     
     for (int i = 0; i < img.rows; i++)
     {
@@ -13,7 +13,7 @@ Mat myFindStraightlines(Mat img) // had to specify it was mine because there is 
         {
             if (enhancedImg.at<uchar>(i,j) == 255)
             {
-                followLine(enhancedImg, i, j);
+                followStraightLine(enhancedImg, i, j);
             }
         }
     }
@@ -22,7 +22,7 @@ Mat myFindStraightlines(Mat img) // had to specify it was mine because there is 
     return enhancedImg;
 }
 
-void followLine(Mat &img, int row, int col)
+void followStraightLine(Mat &img, int row, int col)
 {
     
 }
@@ -31,7 +31,7 @@ void task02(string filename)
 {
     // Load img and apply canny
     Mat img = imread(filename), cannyImg = Mat::zeros(img.size(), CV_8UC1);
-    Canny(img, cannyImg, 540, 410);
+    Canny(img, cannyImg, 400, 400);
 
 
 }
