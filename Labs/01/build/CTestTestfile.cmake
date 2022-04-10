319b10b0b3818403e5749db6d72718b032b58ab0
maxb998 <boldrinmassimo98@gmail.com>
2022-04-10 17:18:23 +0200

    removed build dirs

--- Labs/01/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/massimo/Documents/GitHub/ComputerVision/Labs/01
-# Build directory: /home/massimo/Documents/GitHub/ComputerVision/Labs/01/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Labs/04/build/src/CMakeFiles/tasksLibrary.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tasksLibrary.
-# This may be replaced when dependencies are built.
