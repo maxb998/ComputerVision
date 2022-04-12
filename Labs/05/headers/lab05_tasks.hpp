6af3b142022ae44528b50a2c2cddee3ef5c7eb19
maxb998 <boldrinmassimo98@gmail.com>
2022-04-12 16:55:08 +0200

    lab05 init

---
+++ Labs/05/headers/lab05_tasks.hpp
@@ -0,0 +1,11 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <cmath>
+
+#ifndef lab05_task01
+#define lab05_task01
+
+    void task01(std::string filename);
+
+#endif
---
+++ Labs/05/src/lab05_task01.cpp
@@ -0,0 +1,12 @@
+#include "lab05_tasks.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+void task01(string filename)
+{
+    
+
+    waitKey(0);
+}
