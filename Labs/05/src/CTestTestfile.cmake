5c93dc10974b45cc7d7688c58a74b97be94b14e6
maxb998 <boldrinmassimo98@gmail.com>
2022-04-14 14:43:34 +0200

    less junk in lab 5

--- Labs/05/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/massimo/Documents/GitHub/ComputerVision/Labs/05/src
-# Build directory: /home/massimo/Documents/GitHub/ComputerVision/Labs/05/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
