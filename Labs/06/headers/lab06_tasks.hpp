c9d2a3e7fbba4eb7d96c592195d8966ee13f18cd
maxb998 <boldrinmassimo98@gmail.com>
2022-04-26 17:38:09 +0200

    Lab06 initial commit

---
+++ Labs/06/headers/lab06_tasks.hpp
@@ -0,0 +1,11 @@
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <cmath>
+
+#ifndef lab06_task01
+#define lab06_task01
+
+    void task01(std::string filename1, std::string filename2);
+
+#endif
---
+++ Labs/06/src/lab06_main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include "lab06_tasks.hpp"
+
+using namespace std;
+
+void checkFileExists(string filename);
+
+int main(int argc, char** argv)
+{
+    // Check whether the input arguments are the right amount
+    if (argc != 3)
+    {
+        throw invalid_argument("Usage: <path>/lab04 <task n°> <(OPTIONAL)image path>");
+    }
+    
+    // Check if a files path is specified, otherwise tries to take the image in the default location
+    string imgPath1 = string(argv[1]), imgPath2 = string(argv[2]);
+    checkFileExists(imgPath1);
+    checkFileExists(imgPath2);
+
+    // launch task01
+    task01(imgPath1, imgPath2);
+
+}
+
+void checkFileExists(string filename)
+{
+    FILE *file;
+    
+    if (!(file = fopen(filename.c_str(), "r")))
+        throw invalid_argument(filename + " not found"); 
+    fclose(file);
+}
